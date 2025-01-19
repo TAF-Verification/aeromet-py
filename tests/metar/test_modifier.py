2c52f8229888f66973bd6f54c3bd64a274b1489b
diego-garro <diego.garromolina@yahoo.com>
2025-01-18 20:38:08 -0600

    refactor(tests): metar modifier

--- tests/metar/test_modifier.py
+++ tests/metar/test_modifier.py
@@ -1,41 +1,41 @@
+import pytest
+
+from typing import Dict, Any, Optional
+
 from aeromet_py import Metar
 
-
-def test_auto_modifier():
-    metar = Metar(
-        "METAR KJST 060154Z AUTO 05007KT 10SM OVC085 13/01 A3004 RMK AO2 SLP174 T01280006"
-    )
-
-    assert metar.modifier.code == "AUTO"
-    assert metar.modifier.description == "Automatic report"
-    assert str(metar.modifier) == "automatic report"
-    assert metar.modifier.as_dict() == {"code": "AUTO", "modifier": "Automatic report"}
-
-
-def test_nil_modifier():
-    metar = Metar("METAR KJST 060100Z NIL")
-
-    assert metar.modifier.code == "NIL"
-    assert metar.modifier.description == "Missing report"
-    assert str(metar.modifier) == "missing report"
-    assert metar.modifier.as_dict() == {"code": "NIL", "modifier": "Missing report"}
-
-
-def test_cor_modifier():
-    metar = Metar("METAR MROC 202200Z COR 08011KT 9999 FEW045 SCT200 29/17 A2989 NOSIG")
-
-    assert metar.modifier.code == "COR"
-    assert metar.modifier.description == "Correction"
-    assert str(metar.modifier) == "correction"
-    assert metar.modifier.as_dict() == {"code": "COR", "modifier": "Correction"}
+modifier_metars: Dict[Optional[str], Dict[str, Any]] = {
+    "AUTO": {
+        "code": "METAR KJST 060154Z AUTO 05007KT 10SM OVC085 13/01 A3004 RMK AO2 SLP174 T01280006",
+        "desc": "Automatic report",
+        "dict": {"code": "AUTO", "modifier": "Automatic report"},
+    },
+    "NIL": {
+        "code": "METAR KJST 060100Z NIL",
+        "desc": "Missing report",
+        "dict": {"code": "NIL", "modifier": "Missing report"},
+    },
+    "COR": {
+        "code": "METAR MROC 202200Z COR 08011KT 9999 FEW045 SCT200 29/17 A2989 NOSIG",
+        "desc": "Correction",
+        "dict": {"code": "COR", "modifier": "Correction"},
+    },
+    None: {
+        "code": "SPECI UUDD 152330Z 29005MPS 9999 SCT019 07/04 Q1014 R32L/290042 NOSIG",
+        "desc": None,
+        "dict": {"code": None, "modifier": None},
+    },
+}
 
 
-def test_no_modifier():
-    metar = Metar(
-        "SPECI UUDD 152330Z 29005MPS 9999 SCT019 07/04 Q1014 R32L/290042 NOSIG"
-    )
+@pytest.mark.parametrize("mod,d", [(mod, d) for mod, d in modifier_metars.items()])
+def test_modifier(mod, d):
+    metar = Metar(d.get("code"))
 
-    assert metar.modifier.code == None
-    assert metar.modifier.description == None
-    assert str(metar.modifier) == ""
-    assert metar.modifier.as_dict() == {"code": None, "modifier": None}
+    assert metar.modifier.code == mod
+    assert metar.modifier.description == d.get("desc")
+
+    mod_as_string = d.get("desc").lower() if mod else ""
+    assert str(metar.modifier) == mod_as_string
+
+    assert metar.modifier.as_dict() == d.get("dict")
